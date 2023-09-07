9c1fc8a148cf83844350abea8ab9d2992ed5da1e
kingpin-ay <ayushmondal001@gmail.com>
2023-09-07 19:57:18 +0530

    Base Site is added
    Start using this site as firsthand base site configuration
    just make a pull on the main and you will be able to work on it

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-around">
+        <Button>fuck me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
